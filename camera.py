76f476277583ac74eef132823f36ef135446b4da
Nathan Westfall <nateeastup@gmail.com>
2016-08-28 20:53:48 -0400

    more code cleanup and added a config file for editing

--- camera.py
+++ camera.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from config import Config
 import datetime as dt
 import picamera
 import os
@@ -7,19 +8,15 @@ import shutil
 import json
 import decimal as d
 
-#Setup other settings
-defaultLoc = "/var/www/vids/"
-quality = 20
-minutesPerFile = 5
-port = 9000
-host = "10.1.1.27"
-daysToKeep = 2
+#load in config file
+cfg = Config(file('config.cfg'))
 
 #Setup Flask App Server
 app = Flask(__name__)
 
 #Setup PiCamera
-camera = picamera.PiCamera(resolution=(1280, 720), framerate=24)
+camera = picamera.PiCamera(resolution = (cfg.camera[0]["resolutionWidth"], cfg.camera[0]["resolutionHeight"]), framerate = cfg.camera[0]["framerate"])
+camera.led = cfg.camera[0]["cameraLedDefaultOn"]
 camera.annotate_background = picamera.Color('black')
 currentlyRecording = False
 lastRecordingDuration = 0
@@ -27,21 +24,21 @@ lastRecordingDuration = 0
 """
 When a post method is sent to /record, the system will start recording if it's not already
 """
-@app.route('/record', methods=['POST'])
+@app.route('/record', methods = ['POST'])
 def start_recording():
 	global currentlyRecording
 	if currentlyRecording:
 		return buildJSON(msg = "Already Recording", error = True)
 	currentlyRecording = True
 	lastRecordingStart = dt.datetime.now()
-	record_thread = threading.Thread(target=record_in_background)
+	record_thread = threading.Thread(target = record_in_background)
 	record_thread.start()
 	return buildJSON(msg = "Started Recording")
 
 """
 When a post method is sent to /home, the system wlll stop recording if it's not already
 """
-@app.route('/home', methods=['POST'])
+@app.route('/home', methods = ['POST'])
 def stop_recording():
 	global currentlyRecording
 	if currentlyRecording:
@@ -51,9 +48,19 @@ def stop_recording():
 		return buildJSON(msg = "System was not recording", error = True)
 
 """
-get the last duraction of a recording
+Get the current status of the system
 """
-@app.route('/lastduration', methods=['POST'])
+@app.route('/status', methods = ['GET'])
+def current_status():
+	if currentlyRecording:
+		return buildJSON(msg = "System is currently recording")
+	else:
+		return buildJSON(msg = "System is not recording")
+
+"""
+Get the last duraction of a recording
+"""
+@app.route('/lastduration', methods = ['GET'])
 def last_recording_duration():
 	if lastRecordingDuration == 0:
 		return buildJSON(msg = "No recording has start yet", error = True)
@@ -79,32 +86,41 @@ def record_in_background():
 	while currentlyRecording:
 		start = dt.datetime.now()
 		camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-		camera.start_recording(getFileName(), format='h264', quality = quality)
-		while (d.Decimal((dt.datetime.now() - start).seconds) / d.Decimal(60)) < d.Decimal(minutesPerFile) and currentlyRecording:
+		camera.start_recording(getFileName(), format = cfg.camera[0]["format"], quality = cfg.camera[0]["quality"])
+		camera.led = False
+		ledCount = 0
+		while (d.Decimal((dt.datetime.now() - start).seconds) / d.Decimal(60)) < d.Decimal(cfg.file[0]["daysToKeep"]) and currentlyRecording:
+			ledCount = ledCount + 1
 			camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 			camera.wait_recording(0.5)
+			if ledCount % 3 == 0:
+				camera.led = True
+			else:
+				camera.led = False
 		camera.stop_recording()
 
 	#save last duraction
 	lastRecordingDuration = d.Decimal((dt.datetime.now() - actualstart).seconds) / d.Decimal(60)
+	#turn light back to default status
+	camera.led = cfg.camera[0]["cameraLedDefaultOn"]
 
 """
 get file name for video file using timestamp
 """
 def getFileName():
-	directory = defaultLoc + dt.datetime.now().strftime('%Y%m%d')
+	directory = cfg.file[0]["defaultLocation"] + dt.datetime.now().strftime(cfg.file[0]["defaultFolderNameFormat"])
 	if not os.path.exists(directory):
 		os.makedirs(directory)
-	return directory + '/' + dt.datetime.now().strftime('%Y%m%d%H%M%S') + '.h264'
+	return directory + '/' + dt.datetime.now().strftime(cfg.file[0]["defaultFileNameFormat"]) + '.' + cfg.camera[0]["format"]
 
 """
 clean up old directories
 """
 def cleanUp():
-	past = dt.datetime.now() - dt.timedelta(days=-daysToKeep)
-	directory = defaultLoc + past.strftime('%Y%m%d')
+	past = dt.datetime.now() - dt.timedelta(days = cfg.file[0]["daysToKeep"])
+	directory = cfg.file[0]["defaultLocation"] + past.strftime(cfg.file[0]["defaultFolderNameFormat"])
 	if os.path.exists(directory):
-		shutil.rmtree(directory, ignore_errors=True)
+		shutil.rmtree(directory, ignore_errors = True)
 	return
 
 """
@@ -121,4 +137,4 @@ def buildJSON(msg, error = False):
 
 #Start server
 if __name__ == '__main__':
-	app.run(host= host, port = port)
+	app.run(host = cfg.host[0]["host"], port = cfg.host[0]["port"])
